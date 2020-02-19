f803ede9a9458df92becbe55429fa41a64d61541
ManikandarajV <52148740+ManikandarajV@users.noreply.github.com>
2020-02-19 14:56:48 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,16 @@
 import boto3
-import sys
+# import sys
 
 # secret_key = sys.argv[0]
 # access_key = sys.argv[1]
 # region     = sys.argv[2]
 
-def list_instance():
-	client  = boto3.client("ec2")
-	res = client.describe_instances()
-	print (res)
+# def list_instance():
+client  = boto3.client("ec2")
+res = client.describe_instances()
+print (res)
 
 
-def main():
-	list_instance()
-main()
+# def main():
+# 	list_instance()
+# main()

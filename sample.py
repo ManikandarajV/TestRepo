b80f03dfe9a9d6ab31ccdeee94d84d0010880d31
ManikandarajV <52148740+ManikandarajV@users.noreply.github.com>
2020-02-26 14:28:36 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,15 +1,41 @@
-import urllib2
+import json
+import csv
+import subprocess
 
-def internet_on():
-    try:
-        response=urllib2.urlopen('http://google.comr',timeout=20)
-        return True
-    except urllib2.URLError as err: pass
-    return False
+def parse_json(json_data):
+    data = json.load(open(json_data,"r"))
+    formatted_list=[]
+    amount = []
+    # Format cost by service
+    for bill in data['ResultsByTime']:
+        for group in bill.get('Groups'):
+            print(group.get('Keys')[0])
+            print(group.get('Metrics').get('NetAmortizedCost').get('Amount'))
+            data={}
+            data['service']=group.get('Keys')[0]
+            data['cost']=('{0:.2f}$'.format(float(group.get('Metrics').get('NetAmortizedCost').get('Amount'))))
+            formatted_list.append(data)
+	    print (formatted_list,amount)
+    return formatted_list,amount
 
-def main():
-	if internet_on() == True:
-		print ("yes")
-	else:
-		print ("no")
-main()
+# def generate_csv(data_list,name):
+#     path = '/tmp'
+#     subprocess.call(['chmod', '0777', path])
+#     keys = data_list[0].keys()
+#     file_name = path+"/"+name
+#     with open(file_name, 'w') as output_file:
+#         dict_writer = csv.DictWriter(output_file, keys)
+#         dict_writer.writeheader()
+#         dict_writer.writerows(data_list)
+#     return file_name
+
+# def display(file_name):
+#     with open(file_name, 'r') as output_file:
+#         file_content = output_file.read()
+#     return file_content
+
+
+def lambda_handler(event,context):
+    formatted_list,amount = parse_json("sample.json")
+#     csv = generate_csv(formatted_list,"cost.csv")
+#     print(display(csv))

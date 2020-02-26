864fefff1cb0e8323d3fad8e88f1d718dce24dbf
ManikandarajV <52148740+ManikandarajV@users.noreply.github.com>
2020-02-26 14:23:03 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,15 @@
-print ("My first jenkins build")
+import urllib2
+
+def internet_on():
+    try:
+        response=urllib2.urlopen('http://google.comr',timeout=20)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+
+def main():
+	if internet_on() == True:
+		print ("yes")
+	else:
+		print ("no")
+main()
